0112fab34f13bdc40870ca252b0b8c04ad17778a
jjoedev <jhknuds1@protonmail.com>
2024-10-27 23:39:54 +0100

    CMAKE refactor + Ninja

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,6 @@
-#include <getopt.h>
+#include <iostream>
 
-#include "bar.h"
-
-const char* helpList = "MiBar Help List\n\n-h or --help to show this message again\n-c or --config to change the default config file to a user-defined config file [ -c Monitor2 ]\n";
-
-int main(int argc, char** argv){
-    std::string userCfgFile = "";
-    int option;
-
-    while((option = getopt(argc, argv, ":hc:")) != -1){
-        switch(option){
-        case 'h':
-            std::cout << helpList;
-            return 0;
-        case 'c':
-            userCfgFile = optarg;
-            break;
-        case ':':
-            std::cout << "Option needs a value\n";
-            break;
-        case '?':
-            std::cout << "Unknown option: " << std::string(optarg) << '\n';
-        default:
-            return 0;
-        }
-    }
-
-    MiBar bar(userCfgFile);
-    bar.EventLoop();
-
+int main(){
+    std::cout << "Hello, MiBar - V2\n";
     return 0;
 }
--- src/solInclude.h
+++
@@ -1,7 +0,0 @@
-#ifndef MIBAR_SOL_INCLUDE_H
-#define MIBAR_SOL_INCLUDE_H
-
-#define SOL_ALL_SAFETIES_ON 1
-#include "../external/sol/include/sol/sol.hpp"
-
-#endif
